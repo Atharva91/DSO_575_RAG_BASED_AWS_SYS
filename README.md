38250ea5b4b8d3d8893a6601043da5057ab2cceb
Atharva Deshpande <81283557+Atharva91@users.noreply.github.com>
2024-11-09 10:43:33 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+This is our attempt to Make a project which innvolves RAG Based systems leveraging the most widely Cloud Based services in the world "AWS"
+
+Created By - Atharva Deshpande and Hrishank Chhatbar
---
+++ chatapp.py
@@ -0,0 +1,71 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=50000, chunk_overlap=1000)
+    return text_splitter.split_text(text)
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context. If the answer is not in
+    the provided context, respond with "Answer is not available in the context".
+    \n\nContext:\n{context}\nQuestion:\n{question}\n\nAnswer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    return load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    st.markdown("**Reply:**\n" + response.get("output_text", ""), unsafe_allow_html=True)
+
+def main():
+    st.set_page_config(" RAG Based Systems with AWS Bedrock for Rapid Scaling of Enterprise Applications ", page_icon=":scroll:")
+    st.header(" RAG Based Systems with AWS Bedrock for Rapid Scaling of Enterprise Applications ")
+    user_question = st.text_input("Ask a Question from the PDF Files uploaded ")
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.write("---")
+        st.title("📁 PDF File's Section")
+        pdf_docs = st.file_uploader("Upload your PDF Files & Click on Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process") and pdf_docs:
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+        st.write("---")
+
+if __name__ == "__main__":
+    main()
